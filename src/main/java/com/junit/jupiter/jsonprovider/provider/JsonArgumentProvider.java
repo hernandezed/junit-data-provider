c81a2133a5002999638d0c64fad3539936887a8d
eduardohernandez <eduardo.hernandez@almundo.com>
2018-09-10 23:41:55 -0300

    add license

--- src/main/java/com/junit/jupiter/jsonprovider/provider/JsonArgumentProvider.java
+++ src/main/java/com/junit/jupiter/jsonprovider/provider/JsonArgumentProvider.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2018-2018 Eduardo Hernandez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.junit.jupiter.jsonprovider.provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +27,11 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+/**
+ *  Read arguments for Junit5 test from json file
+ *
+ *  @author Eduardo Hernandez
+ */
 public class JsonArgumentProvider implements ArgumentsProvider, AnnotationConsumer<JsonSource> {
 
     private String[] values;
