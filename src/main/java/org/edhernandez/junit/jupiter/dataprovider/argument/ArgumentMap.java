ed7ff3a5c42a106d29fb590e2be8be6885c22cae
Eduardo Hernandez <eduardo.hernandez@almundo.com>
2019-03-08 00:26:21 -0300

    add typereference to argument map api

--- src/main/java/org/edhernandez/junit/jupiter/dataprovider/argument/ArgumentMap.java
+++ src/main/java/org/edhernandez/junit/jupiter/dataprovider/argument/ArgumentMap.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018-2018 Eduardo Hernandez
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,12 +16,12 @@
 package org.edhernandez.junit.jupiter.dataprovider.argument;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 
 /**
- *
  * Extend hash map and add get method for automatic mapping to specific class
  *
  * @param <T> Key Class
@@ -36,6 +36,13 @@ public class ArgumentMap<T, S> extends HashMap<T, S> {
     }
 
     public <R> R get(Object key, Class<R> aClass) {
+        if (super.get(key) != null) {
+            return objectMapper.convertValue(super.get(key), aClass);
+        }
+        return null;
+    }
+
+    public <R> R get(Object key, TypeReference<R> aClass) {
         if (super.get(key) != null) {
             return objectMapper.convertValue(super.get(key), aClass);
         }
